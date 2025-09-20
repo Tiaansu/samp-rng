04d0e8c3095b411e19d54102ad493328167810a9
Tiaansu <dev.marlon.cabading@gmail.com>
2025-09-20 18:15:29 +0800

    Improvements and try to make the native a 1:1 copy of Pawn's random

--- src/natives.rs
+++ src/natives.rs
@@ -5,7 +5,7 @@ use samp::prelude::*;
 
 impl super::SampRng {
     #[native(raw, name = "RandomNumber")]
-    pub fn random_number(&mut self, _amx: &Amx, mut args: samp::args::Args) -> AmxResult<u32> {
+    pub fn random_number(&mut self, _amx: &Amx, mut args: samp::args::Args) -> AmxResult<i32> {
         let mut rng = thread_rng();
 
         if args.count() == 0 {
@@ -14,29 +14,36 @@ impl super::SampRng {
             return Ok(num);
         } else if args.count() == 1 {
             // max
-            let max_ref = args.next::<Ref<u32>>().ok_or(AmxError::Params)?;
+            let max_ref = args.next::<Ref<i32>>().ok_or(AmxError::Params)?;
             let max = *max_ref;
-            // we are subtracting one so that when we are
-            // using it to get random item from an array,
-            // we won't be getting OOB error.
-            let num = rng.gen_range(0..=max - 1);
-            return Ok(num);
+
+            if max == 0 {
+                let num: u32 = rng.gen();
+                return Ok(num as i32);
+            } else if max > 0 {
+                let num = rng.gen_range(0..max);
+                return Ok(num);
+            } else {
+                // negative to 0.
+                let num = rng.gen_range(max..=0);
+                return Ok(num);
+            }
         } else if args.count() == 2 {
             // min, max
-            let min_ref = args.next::<Ref<u32>>().ok_or(AmxError::Params)?;
-            let max_ref = args.next::<Ref<u32>>().ok_or(AmxError::Params)?;
+            let min_ref = args.next::<Ref<i32>>().ok_or(AmxError::Params)?;
+            let max_ref = args.next::<Ref<i32>>().ok_or(AmxError::Params)?;
 
             let min = *min_ref;
             let max = *max_ref;
-            // same as above
-            let num = rng.gen_range(min..=max - 1);
+
+            let num = rng.gen_range(min..max);
             return Ok(num);
         }
 
-        let mut numbers: Vec<u32> = Vec::new();
+        let mut numbers: Vec<i32> = Vec::new();
 
         for _ in 0..args.count() {
-            let num_ref = args.next::<Ref<u32>>().ok_or(AmxError::Params)?;
+            let num_ref = args.next::<Ref<i32>>().ok_or(AmxError::Params)?;
             let num = *num_ref;
             numbers.push(num);
         }
