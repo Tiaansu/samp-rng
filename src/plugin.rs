b962c058008943a1e6da853abd6fa4e353236d7f
Tiaansu <dev.marlon.cabading@gmail.com>
2025-09-21 14:17:46 +0800

    bump version

--- src/plugin.rs
+++ src/plugin.rs
@@ -5,7 +5,7 @@ pub struct SampRng {}
 
 impl SampPlugin for SampRng {
     fn on_load(&mut self) {
-        info!("Version: 0.0.1");
+        info!("Version: 0.1.1");
     }
 
     fn on_unload(&mut self) {
